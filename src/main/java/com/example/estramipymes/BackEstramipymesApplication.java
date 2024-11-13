e5810f3589cc7843d68bcaa095ff748cca0cdc9f
Miguel Zapata <miguelzj0909@gmail.com>
2024-11-12 19:59:37 -0500

    first commit

---
+++ src/main/java/com/example/estramipymes/BackEstramipymesApplication.java
@@ -0,0 +1,13 @@
+package com.example.estramipymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEstramipymesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEstramipymesApplication.class, args);
+	}
+
+}
