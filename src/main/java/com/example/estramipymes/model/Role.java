9d7ba0c09541ca620adfc041b6cb3efb277a57bd
Esteban Montoya <emontoyab93@gmail.com>
2024-11-12 23:29:51 -0500

    Add models to all entities

--- src/main/java/com/example/estramipymes/model/Role.java
+++ src/main/java/com/example/estramipymes/model/Role.java
@@ -1,5 +1,28 @@
 package com.example.estramipymes.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "admins")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public enum Description{
+        admin, student, profesor, company
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Description description;
     
 }
