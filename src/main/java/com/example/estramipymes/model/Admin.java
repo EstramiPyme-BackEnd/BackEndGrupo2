421231e9c60a476f42083f18c95a3d95e47a8a53
Juan Camilo <juanca2.0@hotmail.com~>
2024-11-19 07:00:38 -0500

    agregando los cambios de admin y company

--- src/main/java/com/example/estramipymes/model/Admin.java
+++ src/main/java/com/example/estramipymes/model/Admin.java
@@ -1,5 +1,17 @@
 package com.example.estramipymes.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Data
+@Table(name = "companies")
 public class Admin {
 
     @Id
